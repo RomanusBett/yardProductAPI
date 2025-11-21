21fe10b539ebb1f82524317b8c08bf46d91f5fac
romanusbett <bettromanus@gmail.com>
2025-11-21 11:06:11 +0300

    project setup

---
+++ src/main/java/com/example/yardProductAPI/YardProductApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.yardProductAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YardProductApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YardProductApiApplication.class, args);
+	}
+
+}
