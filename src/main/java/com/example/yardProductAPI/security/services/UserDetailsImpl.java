7f5068b2ae750894d01e28cec4080bcc30b1a708
romanusbett <bettromanus@gmail.com>
2025-11-24 08:48:04 +0300

    add authentication apis

--- src/main/java/com/example/yardProductAPI/security/services/UserDetailsImpl.java
+++ src/main/java/com/example/yardProductAPI/security/services/UserDetailsImpl.java
@@ -3,20 +3,22 @@ package com.example.yardProductAPI.security.services;
 import com.example.yardProductAPI.models.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
+import java.util.Objects;
 import java.util.Collection;
 import java.util.List;
 
 public class UserDetailsImpl implements UserDetails {
-
+    private static final long serialVersionUID = 1L;
     private Long id;
     private String username;
+    private String email;
     private String password;
 
-    public UserDetailsImpl(Long id, String username, String password) {
+    public UserDetailsImpl(Long id, String username, String password, String email) {
         this.id = id;
         this.username = username;
         this.password = password;
+        this.email = email;
     }
 
  
@@ -24,7 +26,8 @@ public class UserDetailsImpl implements UserDetails {
         return new UserDetailsImpl(
                 user.getId(),
                 user.getUsername(),
-                user.getPassword()
+                user.getPassword(),
+                user.getEmail()
         );
     }
 
@@ -47,6 +50,10 @@ public class UserDetailsImpl implements UserDetails {
         return username;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
     @Override
     public boolean isAccountNonExpired() {
         return true;
@@ -65,5 +72,15 @@ public class UserDetailsImpl implements UserDetails {
     @Override
     public boolean isEnabled() {
         return true;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+    if (this == o)
+        return true;
+        if (o == null || getClass() != o.getClass())
+        return false;
+        UserDetailsImpl user = (UserDetailsImpl) o;
+        return Objects.equals(id, user.id);
     }
 }
